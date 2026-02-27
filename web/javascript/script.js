a01585e79fff3f34bcf0df9e313e634979b3819c
Anas El Badii <anas.elbadii@allievi.itsdigitalcademy.com>
2026-02-27 17:51:57 +0100

    fatto js solom per test login

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -5,14 +5,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('passwordInput').value;
     const errorBox = document.getElementById('error-box');
 
-    // Credenziali di test
+    
     if (email === "admin@easyauto.it" && password === "1234") {
         window.location.href = "confronto.html";
     } else {
-        // Mostra il box d'errore
+       
         errorBox.style.display = "flex";
         
-        // Opzionale: scuote la card per attirare l'attenzione
+        
         const card = document.querySelector('.login-card');
         card.style.animation = "shake 0.4s";
         setTimeout(() => card.style.animation = "", 400);
