301c0cd974bdf1a23aa291d990d35eb26a5aeef1
Simo Gonzato <simo.gonzato@gmail.com>
2026-04-10 14:34:48 +0100

    Update files

--- web/php/getModelData.php
+++ web/php/getModelData.php
@@ -10,7 +10,15 @@ try {
 
     if ($searchTerm !== '') {
         // CORREZIONE 1: Rimosse le virgolette attorno a :name
-        $stmt = $db->prepare("SELECT Auto.modello,Marca.nome as Marca,Auto.anno_Di_Produzione,Auto.immagine,Motore.cilindrata_CC,Motore.cavalli,Motore.alimentazione FROM auto join Motore join Marca on Auto.id_marca = Marca.id_Marca and Auto.id_motore = Motore.id_Motore WHERE Auto.modello = :name");
+        $stmt = $db->prepare("SELECT Auto.modello, 
+        Marca.nome as Marca,
+        Auto.anno_Di_Produzione,
+        Auto.immagine,
+        Motore.cilindrata_CC,
+        Motore.cavalli,
+        Motore.alimentazione 
+        FROM auto join Motore join Marca on Auto.id_marca = Marca.id_Marca and Auto.id_motore = Motore.id_Motore 
+        WHERE Auto.modello = :name");
         
         if (!$stmt) {
             throw new Exception("Errore preparazione query: " . $db->lastErrorMsg());
