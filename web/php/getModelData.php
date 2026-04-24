3c5ffcbeecd3d5042b2efce6918afc2a6eed1aa5
Gabriele Coni <io@domain.tld>
2026-04-24 14:50:01 +0200

    mysql

--- web/php/getModelData.php
+++ web/php/getModelData.php
@@ -1,47 +1,53 @@
 <?php
 header('Content-Type: application/json');
 ini_set('display_errors', 0);
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$db = "easyauto";
 
 try {
-    $db = new SQLite3("../../database/dbautoprova.db");
-
+    $conn = new mysqli($host,$user,$pwd,$db);
+    if ($conn->connect_error) {
+        die(json_encode(["error" => "Connection failed"]));
+    }
     $searchTerm = isset($_GET['name']) ? $_GET['name'] : '';
     $data = []; // Inizializziamo un array vuoto per i risultati
 
     if ($searchTerm !== '') {
         // CORREZIONE 1: Rimosse le virgolette attorno a :name
-        $stmt = $db->prepare("SELECT 
-        Auto.modello,   
-        Marca.nome as Marca,
-        Auto.anno_Di_Produzione,
-        Auto.immagine,
-        Auto.standard_Ambientali,
-        Motore.cilindrata_CC,
-        Motore.cavalli,
-        Motore.alimentazione,
-        Motore.cambio,
-        Motore.trazione,
-        Consumi_100km.consumo,
-        Consumi_100km.tipo
-        FROM auto join Motore join Marca join Consumi_100km join Serie on Auto.id_marca = Marca.id_Marca and Auto.id_motore = Motore.id_Motore and Auto.id_Auto = Consumi_100km.id_auto and Auto.id_serie = Serie.id_Serie
-        WHERE Auto.modello = :name");
+        $stmt = $conn->prepare("SELECT 
+        auto.modello,   
+        marca.nome as Marca,
+        auto.anno_Di_Produzione,
+        /*auto.immagine, TO DO*/
+        auto.standard_Ambientali,
+        motore.cilindrata_CC,
+        motore.cavalli,
+        motore.alimentazione,       
+        motore.cambio,
+        motore.trazione,
+        consumi_100km.consumo,
+        consumi_100km.tipo
+        FROM auto INNER join motore on auto.id_motore = motore.id_Motore INNER join marca on auto.id_marca = marca.id_Marca INNER join consumi_100km on auto.id_Auto = consumi_100km.id_auto INNER join carrozzeria on auto.id_carrozzeria = carrozzeria.id_Carrozzeria
+        WHERE auto.modello = ?");
 
         if (!$stmt) {
-            throw new Exception("Errore SQL: " . $db->lastErrorMsg());
+            throw new Exception("Errore SQL: ");
         }
 
-        $stmt->bindValue(':name', $searchTerm, SQLITE3_TEXT);
-        $result = $stmt->execute();
-
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $data[] = $row;
+        $stmt->bind_param('s', $searchTerm);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                $data[] = $row;
+            }
         }
-    }
-
     echo json_encode($data);
-    $db->close();
-
-} catch (Exception $e) {
+    $conn->close();
+    }
+}catch (Exception $e) {
     // In caso di errore, restituiamo comunque un JSON valido
     echo json_encode(["error" => $e->getMessage()]);
 }
